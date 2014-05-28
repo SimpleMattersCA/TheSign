7927428d517bd5c04a75c8ef6631cbca69605007
achudnovskiy <andrey.chudnvovskiy@gmail.com>
2014-05-28 13:17:02 -0700

    Script that pulls the weather forecast and puts it to Parse https://app.asana.com/0/10530246395848/12332566648022

--- ParseCloudCode/cloud/main.js
+++ ParseCloudCode/cloud/main.js
@@ -1,6 +1,31 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.job("getForecast", function(request, status) {
+
+var apiKey='d9b6767efb140cd602f07adf5a9018af';
+var latitude='49.2759258';
+var longitude='-123.1150547';
+
+Parse.Cloud.httpRequest({
+   url: 'https://api.forecast.io/forecast/'+apiKey+'/'+latitude+','+longitude+'?units=ca',
+  success: function(httpResponse) {
+  	var result = JSON.parse(httpResponse.text);
+  	var WeatherData = Parse.Object.extend("WeatherData");
+  	var newWeather = new WeatherData();
+  	/*console.log(result.currently.temperature);*/
+  	newWeather.save({ currentTemp: result.currently.temperature}).then(function(message) {
+    status.success('Success');
+  }, function(error) {
+   status.error(error)
+  });
+  },
+  error: function(httpResponse) {
+  status,error(httpResponse);
+    console.error('Request failed with response code ' + httpResponse.status);
+  }
+});
+
+
+
 });
