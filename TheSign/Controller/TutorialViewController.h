bd8a70273c0caf2bdc7710406d0549b24f5fc506
achudnovskiy <andrey.chudnvovskiy@gmail.com>
2014-06-05 21:19:42 -0700

    missing two files from Tutorial commit

---
+++ TheSign/Controller/TutorialViewController.h
@@ -0,0 +1,16 @@
+//
+//  TutorialViewController.h
+//  TheSign
+//
+//  Created by Andrey Chudnovskiy on 2014-06-04.
+//  Copyright (c) 2014 Andrey Chudnovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TutorialViewController : UIViewController
+@property NSUInteger pIndex;
+@property NSString* pTitle;
+@property NSString* pText;
+@property UIImage* pImage;
+@end
